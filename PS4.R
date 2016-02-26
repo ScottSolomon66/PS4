8a12bc55a4dae065e33384e526109602fc7afbcf
ScottSolomon66 <scott.solomon66@gmail.com>
2016-02-26 02:23:11 -0600

    First commit
    Just messing with the data, getting it into a data frame, and starting
    to work on sorting it.

--- PS4.R
+++ PS4.R
@@ -8,3 +8,11 @@ temp <- wikiURL %>%
   read_html %>%
   html_nodes("table")
 
+presidential_data<-(html_table(temp[2]))
+
+presidential_data<-as.data.frame(presidential_data)
+
+presidential_data<-presidential_data[3:50,]
+
+presidential_data<-as.data.frame(presidential_data, row.names = presidential_data$Year)
+
