250bb8492233c9a6e8b9cc71647aca14d9588717
ScottSolomon66 <scott.solomon66@gmail.com>
2016-02-29 01:16:20 -0600

    Added all the points to the graph
    I added the republicans and the independents. I know the background of
    the chart is teal. I’m still learning colors

--- PS4.R
+++ PS4.R
@@ -14,30 +14,42 @@ presidential_data<-as.data.frame(presidential_data)
 
 presidential_data<-presidential_data[3:50,]
 
-year<-as.numeric(as.character(presidential_data$Year))
-turnout<-presidential_data$Turnout
-turnout<-as.numeric(sub("%", "", turnout))
-
-turnout_by_year<-cbind(presidential_data$Year, presidential_data$Turnout)
-
-turnout_by_year<-as.matrix(turnout_by_year)
-
+dem_rows<-grep("Dem.", presidential_data$Party)
+dem_data<-presidential_data[dem_rows,]
 dem_years<-dem_data$Year
 dem_turnout<-dem_data$Turnout
 dem_turnout<-as.numeric(sub("%", "", dem_turnout))
 
-plot(NULL, NULL,xlab = "Year", 
+rep_rows<-grep("Rep.", presidential_data$Party)
+rep_data<-presidential_data[rep_rows,]
+rep_years<-rep_data$Year
+rep_turnout<-rep_data$Turnout
+rep_turnout<-as.numeric(sub("%","", rep_turnout))
+
+independent_rows<-c(1, 17, 20)
+independent_data<-presidential_data[independent_rows,]
+independent_years<-independent_data$Year
+independent_turnout<-independent_data$Turnout
+independent_turnout<-as.numeric(sub("%", "", independent_turnout))
+
+
+dem_rep_rows<-c(dem_rows,rep_rows)
+
+
+
+plot(NULL, NULL,
+     xlab = "Year", 
      ylab = "Turnout in Percent", 
      pch = 20,
      main = "Turnout in Percent by Year",
      xlim = c(1800,2020),
      ylim = c(20, 90))
 points(dem_years, dem_turnout, pch = 20, col = 132)
+points(rep_years, rep_turnout, pch = 20, col = 50)
+points(independent_years, independent_turnout, pch = 20, col = "black")
+par(bg = 125)
 
 
-dem_rows<-grep("Dem.", presidential_data$Party)
-
-dem_data<-presidential_data[dem_rows,]
 
 
 
