96a4b443c0a6a0e83ee8c132e30ea72662d70069
ScottSolomon66 <scott.solomon66@gmail.com>
2016-02-28 23:23:04 -0600

    Separated Data, began to plot
    I am starting to separate the democratic and republican data points so
    I can make them different colors in the plot

--- PS4.R
+++ PS4.R
@@ -14,5 +14,30 @@ presidential_data<-as.data.frame(presidential_data)
 
 presidential_data<-presidential_data[3:50,]
 
-presidential_data<-as.data.frame(presidential_data, row.names = presidential_data$Year)
+year<-as.numeric(as.character(presidential_data$Year))
+turnout<-presidential_data$Turnout
+turnout<-as.numeric(sub("%", "", turnout))
+
+turnout_by_year<-cbind(presidential_data$Year, presidential_data$Turnout)
+
+turnout_by_year<-as.matrix(turnout_by_year)
+
+dem_years<-dem_data$Year
+dem_turnout<-dem_data$Turnout
+dem_turnout<-as.numeric(sub("%", "", dem_turnout))
+
+plot(NULL, NULL,xlab = "Year", 
+     ylab = "Turnout in Percent", 
+     pch = 20,
+     main = "Turnout in Percent by Year",
+     xlim = c(1800,2020),
+     ylim = c(20, 90))
+points(dem_years, dem_turnout, pch = 20, col = 132)
+
+
+dem_rows<-grep("Dem.", presidential_data$Party)
+
+dem_data<-presidential_data[dem_rows,]
+
+
 
