183996a6f34499da75613ba028cdcdd3e3b8ae91
ScottSolomon66 <scott.solomon66@gmail.com>
2016-02-29 19:28:44 -0600

    Improving the first plot/making the second plot
    I started working on a second plot and added some new elements for the
    first plot.

--- PS4.R
+++ PS4.R
@@ -32,9 +32,14 @@ independent_years<-independent_data$Year
 independent_turnout<-independent_data$Turnout
 independent_turnout<-as.numeric(sub("%", "", independent_turnout))
 
+turnout<-presidential_data$Turnout
+turnout<-as.numeric(sub("%", "", turnout))
+year<-presidential_data$Year
+
+turnout_regression<-lm(turnout~year)
+turnout_coefficients<-turnout_regression$coefficients
 
 dem_rep_rows<-c(dem_rows,rep_rows)
-
 
 
 plot(NULL, NULL,
@@ -44,12 +49,60 @@ plot(NULL, NULL,
      main = "Turnout in Percent by Year",
      xlim = c(1800,2020),
      ylim = c(20, 90))
-points(dem_years, dem_turnout, pch = 20, col = 132)
-points(rep_years, rep_turnout, pch = 20, col = 50)
-points(independent_years, independent_turnout, pch = 20, col = "black")
-par(bg = 125)
+points(dem_years, dem_turnout, pch = 20, col = 'blue')
+points(rep_years, rep_turnout, pch = 20, col = 'red')
+points(independent_years, independent_turnout, pch = 20, col = "green")
+par(bg = 'white')
+legend(x = 1962, y = 90,
+       legend = c("Democrats", "Republicans", "Other"),
+       pch = 20,
+       col = c('blue', 'red', 'green'),
+       bg = 'grey',
+       cex = .7,
+       title = "Legend",
+       pt.cex = 1)
+abline(a = turnout_coefficients[1], b = turnout_coefficients[2], lty = 2)
+legend(x = 1900, y = 40,
+       legend = "Turnout = 250 - .0976 * Year",
+       cex = .7)
+
+       
+?legend
+
+dem_pop_vote_percent<-dem_data$Popular.vote....
+dem_pop_vote_percent<-as.numeric(sub("%", "", dem_pop_vote_percent))
+
+rep_pop_vote_percent<-rep_data$Popular.vote....
+rep_pop_vote_percent<-as.numeric(sub("%", "", rep_pop_vote_percent))
+
+independent_vote_percent<-independent_data$Popular.vote....
+independent_vote_percent<-as.numeric(sub("%", "", independent_vote_percent))
+
+plot(NULL, NULL,
+     xlab = "Year",
+     ylab = "Winning Vote Percentage",
+     main = "Winning Vote Percentage by Year",
+     ylim = c(20,70),
+     xlim = c(1820, 2020))
+points(dem_years, dem_pop_vote_percent,
+       pch = 20,
+       col = 'blue')
+points(rep_years, rep_pop_vote_percent,
+       pch = 20,
+       col = 'red')
+points(independent_years, independent_vote_percent,
+       pch = 20,
+       col = 'yellow')
+par(bg = 'grey')
 
 
 
+plot(dem_years, dem_pop_vote_percent,
+     main = "Winning Percent",
+     pch = 20,
+     col = 'blue')
+par(bg = 'white')
+
+dem_data$Popular.vote
 
 
